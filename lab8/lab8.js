3cab7d385c61ae8ec08b85f6ca4ab6ef6c53e086
Данил Трохин <trohindanil2004@mail.ru>
2024-05-28 13:00:45 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,12 +1,12 @@
 function showDate() {
     let out = document.getElementById('current-date');
     let today = new Date();
-    out.innerHTML = `Дата и время для русской локали: ${today.toLocaleString('ru-RU')}
-        <div>Дата и время для локали США: ${today.toLocaleString('en-US')}</div>
-        <div>Дата и время для локали Франции: ${today.toLocaleString('fr-FR')}</div>
-        <div>Дата и время для локали Германии: ${today.toLocaleString('de-DE')}</div>
-        <div>Дата и время для локали Китая: ${today.toLocaleString('zh-CN')}</div>
-        <div>Дата и время для локали Японии: ${today.toLocaleString('ja-JP')}</div>
+    out.innerHTML = `Дата и время для русской локали: ${today.toLocaleString('ru-RU')}<br>
+        Дата и время для локали США: ${today.toLocaleString('en-US')}<br>
+        Дата и время для локали Франции: ${today.toLocaleString('fr-FR')}<br>
+        Дата и время для локали Германии: ${today.toLocaleString('de-DE')}<br>
+        Дата и время для локали Китая: ${today.toLocaleString('zh-CN')}<br>
+        Дата и время для локали Японии: ${today.toLocaleString('ja-JP')}<br>
     `;
 }
 
