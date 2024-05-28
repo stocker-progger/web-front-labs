2738d52f17f7577b7ac1ebaa3b11eee7613f282f
Данил Трохин <trohindanil2004@mail.ru>
2024-05-28 12:54:16 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -4,8 +4,25 @@ function showDate() {
     out.innerHTML = `Дата и время для русской локали: ${today.toLocaleString('ru-RU')}
         <div>Дата и время для локали США: ${today.toLocaleString('en-US')}</div>
         <div>Дата и время для локали Франции: ${today.toLocaleString('fr-FR')}</div>
-        <div>Дата и время для локали Германии:${today.toLocaleString('de-DE')}</div>
-        <div>Дата и время для локали Китая:${today.toLocaleString('zh-CN')}</div>
-        <div>Дата и время для локали Японии:${today.toLocaleString('ja-JP')}</div>
+        <div>Дата и время для локали Германии: ${today.toLocaleString('de-DE')}</div>
+        <div>Дата и время для локали Китая: ${today.toLocaleString('zh-CN')}</div>
+        <div>Дата и время для локали Японии: ${today.toLocaleString('ja-JP')}</div>
+    `;
+}
+
+function showCurrentDate() {
+    let dt = new Date();
+    let year = dt.getFullYear();
+    let monthNames = ["январь", "февраль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"];
+    let month = monthNames[dt.getMonth()];
+    let date = dt.getDate();
+    let dayNames = ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
+    let day = dayNames[dt.getDay()];
+
+    document.getElementById('showcurrent-date').innerHTML = `
+        Текущий год: ${year}<br>
+        Текущий месяц: ${month}<br>
+        Текущая дата: ${date}<br>
+        День недели: ${day}
     `;
 }
